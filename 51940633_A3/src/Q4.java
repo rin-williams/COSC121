2f6e76687d8f2bbc612fadaa812df94b2d1be401
Rin <rin@Rins-MacBook-Pro.local>
2023-02-16 14:39:18 -0800

    finished with Q4

--- 51940633_A3/src/Q4.java
+++ 51940633_A3/src/Q4.java
@@ -2,28 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Q4 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         String[] censoredWords = { "ABC", "XYZ" };
 
-        File source = new File("src/source.txt");
-        FileWriter file = new FileWriter("src/destination2.txt");
+        File src = new File("src/source.txt");
+        Scanner in = new Scanner(src);
 
-        Scanner in = new Scanner(source);
+        File des = new File("src/destination2.txt");
+        FileWriter file = new FileWriter(des);
 
         while (in.hasNextLine()) {
             String s = in.nextLine();
-            s.toUpperCase();
-            s.replaceCensoredWords(s, censoredWords);
-            file.write(s);
+            file.write(replaceCensoredWords(s, censoredWords) + "\n");
         }
 
         in.close();
         file.close();
-
     }
 
     private static String replaceCensoredWords(String line, String[] censoredWords) throws FileNotFoundException {
+        Scanner in = new Scanner(line);
+        String toCheck;
+        String toReturn = "";
+        loop: while (in.hasNext()) {
+            toCheck = in.next();
+            for (int j = 0; j < censoredWords.length; j++) {
+                if (censoredWords[j].equalsIgnoreCase(toCheck)) {
+                    toReturn += "... ";
+                    continue loop;
+                }
+            }
+            toReturn += toCheck.toUpperCase() + " ";
 
-        return "Done";
+        }
+        return toReturn;
     }
 }
