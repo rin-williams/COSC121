b0b73b605cb2ad36eaf590eb303f2c418992b25a
Rin <rin@Rins-MacBook-Pro.local>
2023-03-02 18:27:21 -0800

    finished p3

---
+++ 51940633_P3/src/Farm.java
@@ -0,0 +1,206 @@
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+public class Farm {
+
+	private double availableFood;
+	private Animal[] animals;
+	private final int MAX_ANIMAL_COUNT = 100;
+	private int animalsCount = 0;
+
+	// animalsCount is used to track the number of animals in the animals array.
+	// animalsCount should be incremented whenever we add a new animal to animals
+	// array.
+	public Farm(String filename) {
+		animals = new Animal[MAX_ANIMAL_COUNT];
+		load(filename);
+	}
+
+	// public Farm() {
+	// setAvailableFood(1000);
+	// animals = new Animal[MAX_ANIMAL_COUNT];
+	// add(new Chicken());
+	// add(new Cow());
+	// add(new Llama());
+	// add(new Llama());
+
+	// }
+
+	public void makeNoise() { // all animals make their sound (Moo, Cluck, etc)
+		for (Animal animal : getAnimals())
+			animal.sound();
+	}
+
+	public void feedAnimals() { // restore energy of all animals and deduct amount eaten from availableFood
+		for (Animal animal : getAnimals())
+			if (availableFood >= Math.min(animal.getMealAmount(), (100 - animal.getEnergy())))
+				availableFood -= animal.eat();
+			else
+				System.out.println("Not enough food for your animals! You need to collect more food items.");
+	}
+
+	public void animSort() {
+		/*
+		 * sorts animals according to their natural ordering criteria
+		 * Note that we cannot apply Arrays.sort to animals directly if it has null
+		 * values (i.e. when the farm is not full).
+		 */
+		if (animalsCount < MAX_ANIMAL_COUNT) {
+			Animal[] temp = getAnimals();
+			Arrays.sort(temp);
+			System.arraycopy(temp, 0, animals, 0, animalsCount);
+		} else
+			Arrays.sort(animals);
+	}
+
+	public boolean addClone(Animal anim) throws CloneNotSupportedException {
+		// this method creates a clone of an animal and adds it to the list of animals
+		// in the farm
+		return add((Animal) anim.clone());
+	}
+
+	public boolean add(Animal anim) { // add an animal object to animals, return true if added successfully and false
+										// otherwise
+		if (animalsCount < MAX_ANIMAL_COUNT) {
+			animals[animalsCount++] = anim;
+			return true;
+		} else {
+			System.out.println("Farm is full. can't add more animals.");
+			return false;
+		}
+	}
+
+	public void printAnimals() {
+		for (int i = 0; i < animalsCount; i++)
+			System.out.println(animals[i]);
+	}
+
+	public int getNumChicken() {
+		int num = 0;
+		for (Animal a : getAnimals())
+			if (a instanceof Chicken)
+				num++;
+		return num;
+	}
+
+	public int getNumCows() {
+		int num = 0;
+		for (Animal a : getAnimals())
+			if (a instanceof Cow)
+				num++;
+		return num;
+	}
+
+	public int getNumLlamas() {
+		int num = 0;
+		for (Animal a : getAnimals())
+			if (a instanceof Llama)
+				num++;
+		return num;
+	}
+
+	public void printSummary() {
+		System.out.println("The farm has:");
+		System.out.printf("- %d animals (%d Chicken, %d Cows, and %d Llamas)\n", animalsCount, getNumChicken(),
+				getNumCows(), getNumLlamas());
+		System.out.printf("- %.2f units of available food\n", availableFood);
+	}
+
+	public double getAvailableFood() {
+		return availableFood;
+	}
+
+	public void setAvailableFood(double availableFood) {
+		if (availableFood >= 0 && availableFood <= 1000)
+			this.availableFood = availableFood;
+	}
+
+	public Animal[] getAnimals() {
+		// returns an array with only the animals existing in the farm - i.e. doesn't
+		// return the full animals array if it has null values.
+		if (animalsCount < MAX_ANIMAL_COUNT) {
+			Animal[] temp = new Animal[animalsCount];
+			System.arraycopy(animals, 0, temp, 0, animalsCount);
+			return temp;
+		} else
+			return animals;
+	}
+
+	public void exit(String filename) throws FileNotFoundException {
+		File file = new File(filename);
+		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
+			out.writeUTF("availableFood: " + availableFood);
+
+			for (Animal a : getAnimals()) {
+				// store all animals with its corresponding using UTF and Object writing.
+				if (a instanceof Llama) {
+					out.writeUTF("Llama: ");
+					out.writeObject((Animal) a);
+				} else if (a instanceof Chicken) {
+					out.writeUTF("Chicken: ");
+					out.writeObject((Animal) a);
+				} else {
+					out.writeUTF("Cow: ");
+					out.writeObject((Animal) a);
+				}
+			}
+			out.close();
+			System.out.println("----------" + "\nFile saved.\n");
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!\n");
+
+		} catch (IOException e) {
+			System.out.println("Unknown error while exiting! aborted.\n");
+			e.printStackTrace();
+		}
+	}
+
+	public void load(String filename) {
+		File file = new File(filename);
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+			// since we stored available food first, we can read it using readUTF.
+			setAvailableFood(Double.parseDouble(in.readUTF().split(" ")[1]));
+			try {
+				while (in.available() > 0) {
+					String data = in.readUTF();
+					if (data.equals("Cow: ")) {
+						add((Cow) in.readObject());
+					} else if (data.equals("Llama: ")) {
+						add((Llama) in.readObject());
+					} else {
+						add((Chicken) in.readObject());
+					}
+				}
+				System.out.println("\nFile loaded from " + filename + "\n----------");
+			} catch (EOFException e) {
+				System.out.println("Error: end of file.");
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("\nFile not found! Making a default farm." + "\n----------");
+
+			setAvailableFood(1000);
+			animals = new Animal[MAX_ANIMAL_COUNT];
+			add(new Chicken());
+			add(new Cow());
+			add(new Llama());
+			add(new Llama());
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Cannot find class! aborted.\n");
+		} catch (IOException e) {
+			System.out.println("Unknown error while importing ! aborted.\n");
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ 51940633_P3/src/FarmTest.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+
+public class FarmTest {
+	public static void main(String[] args)
+			throws CloneNotSupportedException, FileNotFoundException, ClassNotFoundException {
+		Farm myFarm = new Farm("stat.dat");
+		myFarm.printSummary();
+		for (Animal a : myFarm.getAnimals())
+			a.setEnergy(Math.random() * 100);
+		System.out.println("\nAvailable food before feeding: " + myFarm.getAvailableFood() + "\n");
+		System.out.println("\nInitial list of animals:\n--------------");
+		myFarm.printAnimals();
+		System.out.println("\nAdding a clone of the second animal\n--------------");
+		myFarm.addClone(myFarm.getAnimals()[1]);
+		myFarm.printAnimals();
+		myFarm.feedAnimals();
+		System.out.println("\nAvailable food after feeding: " + myFarm.getAvailableFood() + "\n");
+		System.out.println("\nAfter SORTING:\n--------------");
+		myFarm.animSort();
+		myFarm.printAnimals();
+		System.out.println("\nFarm summary:\n--------------");
+		myFarm.printSummary();
+		myFarm.exit("stat.dat");
+
+		// the reason why we get more animal is because we keep cloning and importing
+		// animals from the stat.dat
+	}
+}
